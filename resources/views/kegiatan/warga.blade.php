6f76cee72504a4fad6b755efdd77623af2afb792
Ariel Saputra <m15759246@gmail.com>
2024-06-01 16:24:23 +0700

    Kegiatan dan Iuran Warga

--- resources/views/kegiatan/warga.blade.php
+++ resources/views/kegiatan/warga.blade.php
@@ -10,6 +10,14 @@
     <div class="pagetitle text-center">
         <h2 class="welcome-message-surat">Kegiatan Warga</h2>
     </div><!-- End Page Title -->
+    <nav>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item active">Home/Kegiatan dan Iuran/ Iuran Warga</li>
+        </ol>
+    </nav>
+    <div class="pagetitle text-center">
+        <h2 class="welcome-message-surat">Kegiatan Warga</h2>
+    </div><!-- End Page Title -->
 
     <section class="section dashboard">
         <div class="row">
@@ -32,6 +40,36 @@
                                 <td>{{ $kegiatan->nama_kegiatan }}</td>
                                 <td>{{ $kegiatan->tanggal_pelaksanaan }}</td>
                                 <td>{{ $kegiatan->tempat_pelaksanaan }}</td>
+                                <td>{{ $kegiatan->penanggung_jawab }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section dashboard">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Kegiatan</th>
+                                <th>Tanggal Pelaksanaan</th>
+                                <th>Tempat</th>
+                                <th>Penanggung Jawab</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($kegiatanWarga as $kegiatan)
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $kegiatan->nama_kegiatan }}</td>
+                                <td>{{ $kegiatan->tanggal_pelaksanaan }}</td>
+                                <td>{{ $kegiatan->tempat }}</td>
                                 <td>{{ $kegiatan->penanggung_jawab }}</td>
                             </tr>
                             @endforeach
