18f67b536f139fabaed6e2dc5e348003da1ea3e7
Dcrezztopia <pascalis.sandi09@gmail.com>
2024-06-02 22:19:27 +0700

    perubahan dikit lagi di users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('username')->unique();
             // $table->string('email')->unique();
             // $table->timestamp('email_verified_at')->nullable();
-            $table->enum('role', ['KRW', 'KRT', 'SRW', 'SRT', 'BRW', 'BRT', 'Warga']);
+            $table->enum('role', ['Admin', 'KRW', 'KRT', 'SRW', 'SRT', 'BRW', 'BRT', 'Warga']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
