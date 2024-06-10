551a75c14f024db410d7902d3b6c460a76710d0d
Pascalis Sandi <pascalis.sandi09@gmail.com>
2024-06-10 22:32:03 +0700

    delete done, update (75%)

--- resources/views/datawarga/index.blade.php
+++ resources/views/datawarga/index.blade.php
@@ -9,6 +9,12 @@
   <section class="section dashboard">
     <div class="row">
       <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-title">Daftar Warga</div>
+                <a href="{{ route('datawarga.create') }}" class="btn btn-primary float-right">Tambah Data</a>
+            </div>
+        </div>
         <div class="table-responsive">
           <table class="w-100">
             <thead>
@@ -34,19 +40,17 @@
                 </td>
                 <td class="cell">{{ $data->tanggal_lahir }}</td>
                 <td class="cell">
-                {{--
-                <!--   <form action="{{ route('pelaporan.destroy', $pelaporan->id_pelaporan) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus pelaporan ini?');"> -->
-                <!--     @csrf -->
-                <!--     @method('DELETE') -->
-                <!--     <button class="btn btn-sm btn-danger" type="submit" -->
-                <!--                 data-bs-toggle="tooltip" -->
-                <!--                 data-bs-placement="top" -->
-                <!--                 title="Hapus laporan" -->
-                <!--     > -->
-                <!--       <i class="bi bi-trash"></i> -->
-                <!--     </button> -->
-                <!--   </form> -->
-                --}}
+                    {{-- Form Edit --}}
+                    <a href="{{ route('datawarga.edit', $data->nik) }}"
+                        class="btn btn-warning btn-sm">Ubah</a>
+                    <!-- Form Delete -->
+                    <form action="{{ route('datawarga.destroy', $data->nik) }}" method="POST"
+                        onsubmit="return confirm('Apakah Anda yakin ingin menghapus data warga ini?');"
+                        style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                    </form>
                 </td>
               </tr>
               @endforeach
@@ -59,8 +63,4 @@
   </section>
     </div>
 </div>
-<<<<<<< HEAD
 @endsection
-=======
-@endsection
->>>>>>> f835ffb (Create Finish)
